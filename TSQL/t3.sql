7cfbfc523c9d5289cac0d559a7452319951a599c
Алексей <jearmix@gmail.com>
2018-02-19 04:06:20 +0200

    sql

--- TSQL/t3.sql
+++ TSQL/t3.sql
@@ -1,8 +1,8 @@
-select u.Id, u.Name
+SELECT u.Id, u.Name
   STUFF(
          (SELECT ', ' + r.RoleName
           FROM roles r
-          where r.User_Id = u.Id
+          WHERE r.User_Id = u.Id
           FOR XML PATH (''))
           , 1, 1, '')  AS roles
-from users u;
+FROM users u;
